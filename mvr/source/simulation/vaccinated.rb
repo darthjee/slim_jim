efdc7c9949b354b103c053aebb72c591113967b4
darthjee <darthjee@gmail.com>
2021-03-26 21:12:51 +0100

    Add rake

--- mvr/source/simulation/vaccinated.rb
+++ mvr/source/simulation/vaccinated.rb
@@ -1,5 +1,5 @@
 class Vaccinated
-  def initialize(population, probability)
+  def initialize(population, infection, mutation)
     @population = population
     @probability = probability
   end
@@ -9,18 +9,20 @@ class Vaccinated
   end
 
   def mutated
-    @mutated ||= Infected.new(infected, 
+    @mutated ||= Infected.new(infected, mutation).mutated
   end
 
   private
 
-  attr_reader :population, :probability
+  attr_reader :population, :infection, :mutation
 
   def calculate
     count = 0
     population.times do
-      count += 1 if Random.rand <= probability
+      count += 1 if Random.rand <= infection
     end
     count
   end
 end
+
+puts Infected
