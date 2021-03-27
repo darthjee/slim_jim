fde6375dc1fe16b3671e2d29919c117bec534b6e
darthjee <darthjee@gmail.com>
2021-03-27 02:21:30 +0100

    Refactor

--- source/mvr/lib/simulation/result.rb
+++ source/mvr/lib/simulation/result.rb
@@ -23,33 +23,27 @@ module Simulation
     delegate :size, to: :results
 
     def infected
-      @infected ||= infected_list.average
+      @infected ||= infected_collection.average
     end
 
     def vaccinated
       @vaccinated ||= vaccinated_collection.average
     end
 
+    def infected_deviance
+      infected_collection.deviance
+    end
+
     def vaccinated_deviance
       vaccinated_collection.deviance
     end
 
+    def infected_collection
+      @infected_collection ||= Collection.new(results.map(&:infected))
+    end
+
     def vaccinated_collection
       @vaccinated_collection ||= Collection.new(results.map(&:vaccinated))
-    end
-
-    def infected_deviance
-      @infected_deviance ||= Math.sqrt(infected_deviances.sum / size)
-    end
-
-    def infected_deviances
-      infected_list.map do |value|
-        (value - infected) ** 2
-      end
-    end
-
-    def infected_list
-      @infected_list ||= results.map(&:infected)
     end
   end
 end
