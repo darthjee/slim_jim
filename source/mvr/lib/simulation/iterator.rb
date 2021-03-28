03a109572fa24cc60e6a575378d4ac09727101d4
darthjee <darthjee@gmail.com>
2021-03-28 10:19:07 +0200

    Add attribute iterator

--- source/mvr/lib/simulation/iterator.rb
+++ source/mvr/lib/simulation/iterator.rb
@@ -1,35 +1,17 @@
 module Simulation
   class Iterator
+    autoload :AttributeIterator, 'simulation/iterator/attribute_iterator'
+
     def initialize(options)
       @options = options
     end
 
     def for_all(key, &block)
-      if (public_send(key))
-        yield(public_send(key))
-      else
-        iterate_over(key, &block)
-      end
-    end
-
-    def iterate_over(key)
-      steps = public_send("#{key}_points")
-      min = public_send("min_#{key}")
-      max = public_send("max_#{key}")
-      delta = max - min
-
-      steps.times do |index|
-        eps = index * delta / (steps - 1)
-        yield(min + eps)
-      end
+      AttributeIterator.new(key, options, &block).iterate
     end
     
     private
 
     attr_reader :options
-
-    delegate :min_infection,  :max_infection,  :infection_points,  :infection,  to: :options
-    delegate :min_mutation,   :max_mutation,   :mutation_points,   :mutation,   to: :options
-    delegate :min_activation, :max_activation, :activation_points, :activation, to: :options
   end
 end
