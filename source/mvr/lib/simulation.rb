497fbe9b571682c48af604e5d8a4d05d8a20c9ef
darthjee <darthjee@gmail.com>
2021-04-01 20:56:06 +0200

    Load mvr main file

--- source/mvr/lib/simulation.rb
+++ source/mvr/lib/simulation.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-lib = File.expand_path(__dir__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-require 'danica'
-require 'safe_attribute_assignment'
-
-autoload :Collection, 'collection'
-autoload :DatFile,    'dat_file'
-autoload :Utils,      'utils'
-
 module Simulation
   autoload :Batch,          'simulation/batch'
   autoload :BatchOptions,   'simulation/batch_options'
--- source/mvr/tasks/main.rake
+++ source/mvr/tasks/main.rake
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../lib/simulation'
+require_relative '../lib/mvr'
 
 import 'mvr/tasks/simulate.rake'
 import 'mvr/tasks/generate_scripts.rake'
