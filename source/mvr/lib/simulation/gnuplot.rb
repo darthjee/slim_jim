da025294eb515af9981ec793022b678506e014a4
darthjee <darthjee@gmail.com>
2021-04-05 20:23:02 +0200

    Move file to correct folder

--- source/mvr/lib/simulation/gnuplot.rb
+++ source/mvr/lib/simulation/gnuplot.rb
@@ -1,27 +1,29 @@
-class Gnuplot
-  private_class_method :new
+module Simulation
+  class Gnuplot
+    private_class_method :new
 
-  def self.generate(*args)
-    new(*args).run
-  end
+    def self.generate(*args)
+      new(*args).run
+    end
 
-  def initialize(output:, template:, **_)
-    @output = output
-    @template_file = template
-  end
+    def initialize(output:, template:, **_)
+      @output = output
+      @template_file = template
+    end
 
-  def generate
-    template.build
-  end
+    def generate
+      template.build
+    end
 
-  private
+    private
 
-  attr_reader :template_file
+    attr_reader :template_file
 
-  def template
-    @template ||= Utils::Template.new(
-      "mvr/templates/#{template_file}.gnu.erb",
-      "mvr/gnuplot/#{output}.gnu"
-    )
+    def template
+      @template ||= Utils::Template.new(
+        "mvr/templates/#{template_file}.gnu.erb",
+        "mvr/gnuplot/#{output}.gnu"
+      )
+    end
   end
 end
